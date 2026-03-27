ca83d14ca954e5842c50357b34847fbc396e9eb0
Naomi Uwensuyi <naomi.uwensuyi@students.bfh.ch>
2026-03-27 14:04:31 +0100

    Default Funktionen erstellt (Datenbank)

--- Datenbank/DatenbankManager.py
+++ Datenbank/DatenbankManager.py
@@ -1,3 +1,21 @@
+
 class DatenbankManager:
-    def __init__(self):
-        pass
+    # Verbindung & Setup
+    def __init__(self)
+    def verbinden(self)
+    def tabellen_erstellen(self)
+    
+    # Buch-Methoden
+    def buch_speichern(self)
+    def buch_laden(self)
+    def bucher_suchen(self)
+    # ... etc
+    
+    # User-Methoden
+    def user_speichern(self)
+    def user_laden(self)
+    # ... etc
+    
+    # Ausleihen-Methoden
+    def ausleih_speichern(self)
+    # ... etc
