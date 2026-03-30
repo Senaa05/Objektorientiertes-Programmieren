093e1682f3b486ab4b22ff1f7272532953853394
Sena Besir <sena.besir@students.fhnw.ch>
2026-03-30 21:47:16 +0200

    init Buch

---
+++ Backend/modelle/buch.py
@@ -0,0 +1,7 @@
+# Klasse erstellen für Buch
+class Buch:
+    def __init__(self, titel, autor, isbn, jahr):
+        self.titel = titel
+        self.autor = autor
+        self.isbn = isbn
+        self.jahr = jahr    
--- Datenbank/DatenbankManager.py
+++ Datenbank/DatenbankManager.py
@@ -1,21 +1,35 @@
 
 class DatenbankManager:
     # Verbindung & Setup
-    def __init__(self)
-    def verbinden(self)
-    def tabellen_erstellen(self)
+    def __init__(self):
+        pass
+
+    def verbinden(self):
+        pass
+
+    def tabellen_erstellen(self):
+        pass
     
     # Buch-Methoden
-    def buch_speichern(self)
-    def buch_laden(self)
-    def bucher_suchen(self)
+    def buch_speichern(self):
+        pass
+
+    def buch_laden(self):
+        pass
+
+    def bucher_suchen(self):
+        pass
     # ... etc
     
     # User-Methoden
-    def user_speichern(self)
-    def user_laden(self)
+    def user_speichern(self):
+        pass
+
+    def user_laden(self):
+        pass
     # ... etc
     
     # Ausleihen-Methoden
-    def ausleih_speichern(self)
+    def ausleih_speichern(self):
+        pass
     # ... etc
