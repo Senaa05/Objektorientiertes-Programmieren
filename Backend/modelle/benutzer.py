0a167bfb56fb9866c7232d87aa8e4fa2517b529a
Sena Besir <sena.besir@students.fhnw.ch>
2026-03-30 22:04:39 +0200

    initial Klassenstruktur Benutzer

---
+++ Backend/modelle/benutzer.py
@@ -0,0 +1,8 @@
+# Klasse für Bibliothek Standardbenutzer
+class User:
+    pass
+
+# Klasse für Administratoren
+# Vererbung von User und besitzt zusätzliche Rechte und Funktionen
+class Administrator(User):
+    pass
