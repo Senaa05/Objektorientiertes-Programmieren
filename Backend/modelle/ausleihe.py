3c13f44825b5aeb188c74d07f256f2fe2aaa43c5
Sena Besir <sena.besir@students.fhnw.ch>
2026-04-08 10:28:43 +0200

    init Klassenstruktur Ausleihe

---
+++ Backend/modelle/ausleihe.py
@@ -0,0 +1,15 @@
+# Bestimmte Funktionen des Moduls DateTime importieren
+from datetime import date, timedelta
+# Klasse für Buch Ausleihe
+class Ausleihe:
+    def __init__(self, user, exemplar):
+        self.user = user
+        self.exemplar = exemplar
+        # Ausleihdatum als den heutigen Datum festlegen
+        self.ausleihdatum = date.today()
+        # Fälligkeit des Buches in 30 Tagen
+        self.faelligkeit = ausleihdatum + timedelta(days=30)
+        self.verlaengern = 0
+        self.rueckgabedatum = None
+        
+      
